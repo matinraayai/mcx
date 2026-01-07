99d6b1a27151bf751efcf59f4e276a4e60d3cd97
matinraayai <30674652+matinraayai@users.noreply.github.com>
2026-01-07 11:21:07 -0500

    Formatting.

--- mcx/mcx_core.cu
+++ mcx/mcx_core.cu
@@ -6,24 +6,25 @@
 **
 **  \section sref Reference
 **  \li \c (\b Fang2009) Qianqian Fang and David A. Boas,
-**          <a href="http://www.opticsinfobase.org/abstract.cfm?uri=oe-17-22-20178">
-**          "Monte Carlo Simulation of Photon Migration in 3D Turbid Media Accelerated
-**          by Graphics Processing Units,"</a> Optics Express, 17(22) 20178-20190 (2009).
-**  \li \c (\b Yu2018) Leiming Yu, Fanny Nina-Paravecino, David Kaeli, and Qianqian Fang,
-**          "Scalable and massively parallel Monte Carlo photon transport
-**           simulations for heterogeneous computing platforms," J. Biomed. Optics,
-**           23(1), 010504, 2018. https://doi.org/10.1117/1.JBO.23.1.010504
-**  \li \c (\b Yan2020) Shijie Yan and Qianqian Fang* (2020), "Hybrid mesh and voxel
-**          based Monte Carlo algorithm for accurate and efficient photon transport
-**          modeling in complex bio-tissues," Biomed. Opt. Express, 11(11)
-**          pp. 6262-6270. https://doi.org/10.1364/BOE.409468
+**  <a href="http://www.opticsinfobase.org/abstract.cfm?uri=oe-17-22-20178">
+**  "Monte Carlo Simulation of Photon Migration in 3D Turbid Media Accelerated
+**  by Graphics Processing Units,"</a> Optics Express, 17(22) 20178-20190
+**   (2009).
+**  \li \c (\b Yu2018) Leiming Yu, Fanny Nina-Paravecino, David Kaeli, and
+**  Qianqian Fang, "Scalable and massively parallel Monte Carlo photon transport
+**  simulations for heterogeneous computing platforms," J. Biomed. Optics,
+**  23(1), 010504, 2018. https://doi.org/10.1117/1.JBO.23.1.010504
+**  \li \c (\b Yan2020) Shijie Yan and Qianqian Fang* (2020), "Hybrid mesh and
+**  voxel based Monte Carlo algorithm for accurate and efficient photon
+**  transport modeling in complex bio-tissues," Biomed. Opt. Express, 11(11)
+**  pp. 6262-6270. https://doi.org/10.1364/BOE.409468
 **
 **  \section sformat Formatting
-**          Please always run "make pretty" inside the \c src folder before each commit.
-**          The above command requires \c astyle to perform automatic formatting.
+**  Please always run "make pretty" inside the \c src folder before each commit.
+**  The above command requires \c astyle to perform automatic formatting.
 **
 **  \section slicense License
-**          GPL v3, see LICENSE.txt for details
+**           GPL v3, see LICENSE.txt for details
 ********************************************************************************
  \file    mcx_core.cu
 
